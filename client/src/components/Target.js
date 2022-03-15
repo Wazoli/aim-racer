7e9d2f94baefaacd4dbb9bf76eacd1190b276a85
Devaka Dias <87965434+Wazoli9@users.noreply.github.com>
2022-03-16 02:09:35 +0530

    set up socket io client

--- client/src/components/Target.js
+++ client/src/components/Target.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function Target(props){
-    console.log(props.targetSize)
     return(
         <div onClick={props.handleClick} style = {{width : props.targetSize, height : props.targetSize, visibility: props.visibility}} className = {props.id + ' circle'}></div>
     )
