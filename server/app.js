29e1a3fbc4e17a08a4bd85ce710cd6cbcc6450c0
Devaka Dias <devaka.dias@gmail.com>
2022-08-22 17:49:52 +0530

    init

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:3000",
-        // origin: 'https://aimracer.herokuapp.com/',
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    // origin: "http://localhost:3001",
+    origin: "https://aimracer.herokuapp.com",
+    methods: ["GET", "POST"],
+  },
 });
 
 const path = require("path");
@@ -19,91 +19,86 @@ let socketToRoomMap = {};
 let roomToPlayerCountMap = {};
 
 io.on("connection", (socket) => {
-    console.log(playerCount + " joined");
-    let joinedRoom = false;
-    for (const room in roomToPlayerCountMap) {
-        if (roomToPlayerCountMap[room] < 2) {
-            let newRoom = parseInt(room);
-            socket.join(newRoom);
-            socketToRoomMap[socket.id] = parseInt(newRoom);
-            roomToPlayerCountMap[newRoom] += 1;
-            joinedRoom = true;
-            break;
-        }
+  console.log(playerCount + " joined");
+  let joinedRoom = false;
+  for (const room in roomToPlayerCountMap) {
+    if (roomToPlayerCountMap[room] < 2) {
+      let newRoom = parseInt(room);
+      socket.join(newRoom);
+      socketToRoomMap[socket.id] = parseInt(newRoom);
+      roomToPlayerCountMap[newRoom] += 1;
+      joinedRoom = true;
+      break;
     }
-    if (!joinedRoom) {
-        let newRoom = 1000
-        while(roomToPlayerCountMap[newRoom]){
-            newRoom += 1
-        }
-        socket.join(newRoom);
-        socketToRoomMap[socket.id] = newRoom;
-        roomToPlayerCountMap[newRoom] = 1;
+  }
+  if (!joinedRoom) {
+    let newRoom = 1000;
+    while (roomToPlayerCountMap[newRoom]) {
+      newRoom += 1;
     }
+    socket.join(newRoom);
+    socketToRoomMap[socket.id] = newRoom;
+    roomToPlayerCountMap[newRoom] = 1;
+  }
 
-    console.log('connected')
-    console.log(roomToPlayerCountMap)
+  console.log("connected");
+  console.log(roomToPlayerCountMap);
 
-    socket.emit("roomChanged", {room : socketToRoomMap[socket.id]})
+  socket.emit("roomChanged", { room: socketToRoomMap[socket.id] });
 
-    socket.on("scoreUpdate", (data) => {
-        socket.broadcast
-            .to(socketToRoomMap[socket.id])
-            .emit("opponentScoreUpdate", data);
-    });
+  socket.on("scoreUpdate", (data) => {
+    socket.broadcast
+      .to(socketToRoomMap[socket.id])
+      .emit("opponentScoreUpdate", data);
+  });
 
-    socket.on('roomRequest', (data)=>{
-        console.log('before room change')
-        console.log(roomToPlayerCountMap)
-        currRoom = socketToRoomMap[socket.id]
-        if(!roomToPlayerCountMap[data.room]){
-            roomToPlayerCountMap[currRoom] -= 1
-            roomToPlayerCountMap[data.room] = 1
-            socketToRoomMap[socket.id] = data.room
-            socket.join(data.room)
-            socket.emit("roomChanged", data)
-        }
-        else if(roomToPlayerCountMap[data.room] < 2){
-            roomToPlayerCountMap[currRoom] -= 1
-            socketToRoomMap[socket.id] = data.room
-            roomToPlayerCountMap[data.room] += 1
-            socket.join(data.room)
-            socket.emit("roomChanged", data)
-        }
-        console.log('after room change')
-        console.log(roomToPlayerCountMap)
-    })
+  socket.on("roomRequest", (data) => {
+    console.log("before room change");
+    console.log(roomToPlayerCountMap);
+    currRoom = socketToRoomMap[socket.id];
+    if (!roomToPlayerCountMap[data.room]) {
+      roomToPlayerCountMap[currRoom] -= 1;
+      roomToPlayerCountMap[data.room] = 1;
+      socketToRoomMap[socket.id] = data.room;
+      socket.join(data.room);
+      socket.emit("roomChanged", data);
+    } else if (roomToPlayerCountMap[data.room] < 2) {
+      roomToPlayerCountMap[currRoom] -= 1;
+      socketToRoomMap[socket.id] = data.room;
+      roomToPlayerCountMap[data.room] += 1;
+      socket.join(data.room);
+      socket.emit("roomChanged", data);
+    }
+    console.log("after room change");
+    console.log(roomToPlayerCountMap);
+  });
 
-    socket.on('playerReady', (data)=>{
-        socket.broadcast
-            .to(socketToRoomMap[socket.id])
-            .emit("playerReady", data);
-    })
+  socket.on("playerReady", (data) => {
+    socket.broadcast.to(socketToRoomMap[socket.id]).emit("playerReady", data);
+  });
 
-    socket.on('confirmReady', (data)=>{
-        socket.broadcast
-            .to(socketToRoomMap[socket.id])
-            .emit("confirmReady", data);
-    })
+  socket.on("confirmReady", (data) => {
+    socket.broadcast.to(socketToRoomMap[socket.id]).emit("confirmReady", data);
+  });
 
-    socket.on("disconnecting", () => {
-        roomToPlayerCountMap[socketToRoomMap[socket.id]] -= 1;
-        console.log('disconnecting')
-        console.log(roomToPlayerCountMap)
-    });
+  socket.on("disconnecting", () => {
+    roomToPlayerCountMap[socketToRoomMap[socket.id]] -= 1;
+    console.log("disconnecting");
+    console.log(roomToPlayerCountMap);
+  });
 
-    socket.on("disconnect", () => {  
-        delete socketToRoomMap[socket.id];
-    });
-    playerCount++;
+  socket.on("disconnect", () => {
+    delete socketToRoomMap[socket.id];
+  });
+  playerCount++;
 });
 
 httpServer.listen(port, () => {
-    console.log("listening on " + port);
+  console.log("listening on " + port);
 });
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
